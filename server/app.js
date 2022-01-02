20936d82cc8bc4b967037c89bc2dc09b89e5050c
jonathan12094 <jon@orlandospencer.com>
2022-01-01 21:35:56 -0500

    set express app

--- server/app.js
+++ server/app.js
@@ -1,15 +1,29 @@
+// loads .env file contents into https
+require("dotenv").config()
 const express = require('express') // initializing express 
-const bodyParser = require('body-parser') // parse data
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
+const bodyParser = require('body-parser') // parse incoming requests data
 const app = express()
-
-const routesTesting = require("./routes/testing") // importing routes files
-
-app.use(routesTesting);
-
-
-app.use(bodyParser.urlencoded({ extended: false })) // this is if it should be able to parse non-default features you could say,
+const port = process.env.PORT
+app.set("port", port)
+app.use(bodyParser.urlencoded({ extended: false })) // etended, this is if it should be able to parse non-default features you could say,
 // app.use(bodyParser.urlencoded()) // only parse data recieve from forms, json 
 
+app.use(require("./routes"))
 
-app.listen(3000)
+let connectionString = "mongodb://localhost:27017/practing_nodejs"
+
+mongoose.connect(connectionString)
+    .then(result => {
+        console.log('Connected To Database Successfully!')
+    })
+    .catch(err => console.log("Error ", err))
+
+
+app.listen(port, () => {
+    console.log(`Server listining on port ${port}`)
+})
+
+module.exports = app
