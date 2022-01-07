f53133800b8e122970f58dfd99a3145e76cf6bf3
jonathanj101 <jonatjimen@gmail.com>
2022-01-06 23:25:52 -0500

    set index.js for controllers and setting up routes for user, testing

--- server/controller/index.js
+++ server/controller/index.js
@@ -2,6 +2,11 @@ const express = require("express")
 
 const controllerRoutes = express.Router()
 
-controllerRoutes.use('/login', require("./userAuth/userAuthRoutes"))
+// controllerRoutes.use('/', (req, res, next) => {
+//     console.log("/")
+//     return res.send("<h1>hello</h1>")
+
+// })
+controllerRoutes.use('/user', require("./userAuth/userAuthRoutes"))
 
 module.exports = controllerRoutes
