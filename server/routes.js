65b6425790dcd5f56f692c2caf5c18e68b8b40db
jonathanj101 <jonatjimen@gmail.com>
2022-01-06 23:27:10 -0500

    setting up routes

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,8 @@
 const express = require("express")
 const route = express.Router() // initializing express router. for routes purposes
 
-route.use('/', require('./controller/')) // on / route, app use controller routes
 
-// route.use("/", (req, res, next) => {
-//     console.log("/")
-//     return res.send("<h1>hello</h1>")
-// })
+route.use('/', require('./controller')) // on / route, app use controller routes
+// route.use('/user', require("./controller"))
+
 module.exports = route
