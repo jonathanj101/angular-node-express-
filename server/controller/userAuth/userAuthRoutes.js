09d91a61ef68b9514409e0b1737ba3b08b2c4f91
jonathanj101 <jonatjimen@gmail.com>
2022-01-06 23:25:06 -0500

    set up testing, testing

--- server/controller/userAuth/userAuthRoutes.js
+++ server/controller/userAuth/userAuthRoutes.js
@@ -1,7 +1,7 @@
 // user controller
 const express = require("express")
-const userController = require("./userAuthController")
 const userRoute = express.Router()
+const userController = require("./userAuthController")
 
 
 // Login
@@ -21,6 +21,25 @@ const loginMiddleWare = [
     userController.login
 ];
 userRoute.post('/login', loginMiddleWare)
+
+// Register
+const registerMiddleWare = [
+    (req, res, next) => {
+        const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const username = req.body.username;
+        const password = req.body.password;
+
+        if (firstName == '' && lastName == '', username == '', password == '') {
+            return res.status(404).json({
+                msg: "no empty info."
+            })
+        }
+        return next()
+    },
+    userController.register
+];
+userRoute.post('/register', registerMiddleWare)
 
 
 module.exports = userRoute
