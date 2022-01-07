7b9ad3bb2ccf4ae47a7807685df999c1c696be4a
jonathanj101 <jonatjimen@gmail.com>
2022-01-06 23:24:43 -0500

    set up services, testing

--- server/controller/userAuth/userAuthServices.js
+++ server/controller/userAuth/userAuthServices.js
@@ -1,1 +1,13 @@
 // user services
+
+const userModel = require("../../model/User")
+
+const userServices = {}
+
+userServices.register = (data) => {
+    console.log('user registration data', data)
+    return userModel.create(data)
+}
+
+
+module.exports = userServices
