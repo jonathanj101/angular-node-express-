55d0b46d99d33a9a3d4d0c250b5f8379020c5898
jonathanj101 <jonatjimen@gmail.com>
2022-01-06 23:27:31 -0500

    setting up main server file

--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
 // loads .env file contents into https
 require("dotenv").config()
 const express = require('express') // initializing express 
+const http = require('http')
 const mongoose = require("mongoose")
-const Schema = mongoose.Schema
 
 const bodyParser = require('body-parser') // parse incoming requests data
 const app = express()
 const port = process.env.PORT
 app.set("port", port)
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: false })) // etended, this is if it should be able to parse non-default features you could say,
 // app.use(bodyParser.urlencoded()) // only parse data recieve from forms, json 
 
@@ -17,13 +23,13 @@ let connectionString = "mongodb://localhost:27017/practing_nodejs"
 
 mongoose.connect(connectionString)
     .then(result => {
+        const server = http.createServer(app)
+        server.listen(port, () => {
+            console.log(`Server listining on port ${port}`)
+        })
         console.log('Connected To Database Successfully!')
-    })
-    .catch(err => console.log("Error ", err))
+    }).catch(err => console.log("Error ", err))
 
 
-app.listen(port, () => {
-    console.log(`Server listining on port ${port}`)
-})
 
 module.exports = app
