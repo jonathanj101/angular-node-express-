bd677d8ae5a064262a0ef026d7a5a4314e78c743
jonathanj101 <jonatjimen@gmail.com>
2022-01-06 23:24:20 -0500

    set up controller, testing

--- server/controller/userAuth/userAuthController.js
+++ server/controller/userAuth/userAuthController.js
@@ -3,7 +3,7 @@ const userServices = require('./userAuthServices')
 //  Login
 const userController = {}
 
-userController.login = (req, res, next) => {
+userController.login = async (req, res, next) => {
     console.log("user controller login")
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
@@ -14,6 +14,32 @@ userController.login = (req, res, next) => {
         console.log("log in worked")
         return res.status(200).json({
             msg: "login successful"
+        })
+    } else {
+        return res.status(404).json({
+            msg: "error"
+        })
+    }
+}
+
+userController.register = async (req, res, next) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const username = req.body.username;
+    const password = req.body.password;
+    if (firstName !== "" && lastName !== "" && username !== "" && password !== "") {
+        console.log("registraion conditionals")
+        const data = {
+            firstName: firstName,
+            firstName: firstName,
+            lastName: lastName,
+            username: username,
+            password: password,
+        }
+        const userModel = await userServices.register(data)
+        console.log("userModel", userModel.toJSON())
+        return res.status(200).json({
+            msg: "registration successful"
         })
     } else {
         return res.status(404).json({
