ec07b553255c089bf57bf1d4ae48f2d8916b51be
jonathanj101 <jonatjimen@gmail.com>
2022-01-09 19:27:52 -0500

    added login logic

--- server/controller/userAuth/userAuthServices.js
+++ server/controller/userAuth/userAuthServices.js
@@ -4,10 +4,14 @@ const userModel = require("../../model/User")
 
 const userServices = {}
 
-userServices.register = (data) => {
-    console.log('user registration data', data)
-    return userModel.create(data)
+userServices.login = (username) => {
+    console.log('userServies.login data', username)
+    return userModel.findOne({ username: username })
 }
 
+userServices.register = (data) => {
+    console.log('userServices.register data', data)
+    return userModel.create(data)
+}
 
 module.exports = userServices
